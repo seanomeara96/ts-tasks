80acac2de1ee9e44eeeeb92a5c932db2c62db45b
Sean <seanom96@gmail.com>
2020-05-06 23:49:01 +0100

    getAllTasks function

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,8 +1,10 @@
-import { Controller } from '@nestjs/common';
-import {TasksService} from './tasks.service'
+import { Controller, Get } from '@nestjs/common';
+import { TasksService } from './tasks.service'
 @Controller('tasks')
 export class TasksController {
-    constructor(private tasksService: TasksService){
-
+    constructor(private tasksService: TasksService){}
+    @Get() // This tells the function below to handle get requests to /tasks
+    getAllTasks(){ // this.taskService was created in the constructor
+        return this.tasksService.getAllTasks()
     }
 }
