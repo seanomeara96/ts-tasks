2f12b2c33df05954f996d8df9dd52b6b8658525d
seanomeara96 <seanom96@gmail.com>
2020-06-08 21:05:24 +0100

    user can patch tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,13 +1,16 @@
-import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch, Query } from '@nestjs/common';
 import { TasksService } from './tasks.service'
 import { Task } from './tasks.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { get } from 'http';
+import { GetTaskFilterDto } from './dto/get-task-filter.dto';
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService: TasksService){}
     @Get() // This tells the function below to handle get requests to /tasks
-    getAllTasks() :Task[] {  // Returns an array of type Task
+    getTasks(@Query() filterDto: GetTaskFilterDto) :Task[] { 
+        
+        console.log(filterDto);
         // this.taskService was created in the constructor
         return this.tasksService.getAllTasks()
     }
@@ -23,5 +26,9 @@ export class TasksController {
     @Delete("/:id")
     deleteTask(@Param("id") id: string): Task[] {
         return this.tasksService.deleteTask(id)
+    }
+    @Patch("/:id/:key")
+    updateTask(@Param("id") id: string, @Param("key") key: string, @Body("newVal") newVal) {
+        return this.tasksService.updateTask(id, key, newVal)
     }
 }
